87fa0c631ac3ccf28e9c2bfacaaad76795fb34e5
Tori <cameroninoz@gmail.com>
2019-07-23 15:41:50 -0400

    wrote time_lapse.py

--- time_lapse.py
+++ time_lapse.py
@@ -8,17 +8,23 @@ import time
 
 
 # Enter the path where the raspberry pi will store its file
-RASPI_PATH = ""
+RASPI_PATH = "/home/pi"
 
 INTERVAL = None# the time interval (in seconds) between pictures
 SESSION_LENGTH = None# the duration of the script
 
 camera = PiCamera()
 camera.start_preview()
-
-
 # Have the camera take pictures at the specified interval until the session is over
 
+#this creates a folder everytime we start the camera so we can have a collection per run- the folder is named after the start date and time
+FOLDERCREATED = datetime.datetime.now()
+
+#inside the () is the number of photos taken
+for i in range(SESSION_LENGTH/INTERVAL):
+	time.sleep(INTERVAL)
+	#this captures an image and saves into the FOLDERCREATED, the images are named image(i).jpg
+	camera.capture('{}/{}/image{}.jpg'.format(RASPI_PATH, FOLDERCREATED, i))
 
 camera.stop_preview()
 
